ffd18aa113d16616b71a67a54cc5867870017e53
Denis Stepanenko <stepanenkodo@yandex.ru>
2025-01-24 17:20:54 +0300

    change navigation

---
+++ CarPark/CarPark/Pages/Shared/_Navigation.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CarPark.Pages.Shared
+{
+    public class _NavigationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
