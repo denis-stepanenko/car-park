4add7bdac9e67b9bb6f4e612591d0c2349d23f69
Denis Stepanenko <stepanenkodo@yandex.ru>
2025-01-24 16:45:40 +0300

    add helper to extract anti forgery token from pages

---
+++ CarPark/CarPark.IntegrationTests/Infrastructure/AntiForgeryTokenExtractor.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CarPark.IntegrationTests.Infrastructure
+{
+    public static class AntiForgeryTokenExtractor
+    {
+        public static string Field { get; } = "AntiForgeryField";
+
+        public static async Task<string> Extract(HttpResponseMessage response)
+        {
+            var html = await response.Content.ReadAsStringAsync();
+
+            var requestVerificationTokenMatch = Regex.Match(html, $@"\<input name=""{Field}"" type=""hidden"" value=""([^""]+)"" \/\>");
+
+            if (requestVerificationTokenMatch.Success)
+                return requestVerificationTokenMatch.Groups[1].Captures[0].Value;
+            else
+                throw new ArgumentException($"Anti forgery token '{Field}' не найден", nameof(html));
+        }
+    }
+}
