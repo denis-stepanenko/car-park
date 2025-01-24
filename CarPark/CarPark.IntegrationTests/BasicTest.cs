c5a5d3131e0ac361a3b935f9275879df979cf46e
Denis Stepanenko <stepanenkodo@yandex.ru>
2025-01-24 16:50:55 +0300

    add basic test

---
+++ CarPark/CarPark.IntegrationTests/BasicTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace CarPark.IntegrationTests
+{
+    public class BasicTest : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public BasicTest(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("/")]
+        [InlineData("/Index")]
+        public async Task Should_ReturnSuccessAndCorrectContentType_WhenCallEveryEndpoint(string url)
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync(url);
+
+            response.EnsureSuccessStatusCode();
+
+            Assert.Equal("text/html; charset=utf-8", response?.Content?.Headers?.ContentType?.ToString());
+        }
+    }
+}
